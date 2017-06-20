66c7e164733153a8334372fa9d94dfc2430147b7
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-06-20 16:36:11 +0200

    fixed fail because of source

--- tests/travis-build.sh
+++ tests/travis-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -ex
+source /opt/qt*/bin/qt*-env.sh
+set -e
 
 echo QMAKE_CXX = g++-7 >> .qmake.conf
 
-source /opt/qt*/bin/qt*-env.sh
 /opt/qt*/bin/qmake
 make qmake_all
 make -j$(nproc) all
