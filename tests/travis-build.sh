a8438e603f38c7d823b98a03e9fe2a70ae9e5e4c
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-06-20 15:31:31 +0200

    newer gcc and run tests

--- tests/travis-build.sh
+++ tests/travis-build.sh
@@ -4,3 +4,8 @@ source /opt/qt*/bin/qt*-env.sh
 /opt/qt*/bin/qmake
 make qmake_all
 make -j$(nproc) all
+
+cd tests/auto
+for test in $(find . -name "tst_*"); do
+	$test
+done
--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository --yes ppa:beineri/opt-qt59-trusty
 sudo apt-get update -qq
-sudo apt-get -qq install qt59base
+sudo apt-get -qq install g++-7 qt59base
