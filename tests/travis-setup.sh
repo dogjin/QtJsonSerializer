4a9393d8de14713cfc83ba58f2138d055c99ab99
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-06-20 15:09:19 +0200

    .

--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-sudo add-apt-repository --yes ppa:beineri/opt-qt59-xenial
+sudo add-apt-repository --yes ppa:beineri/opt-qt59-trusty
 sudo apt-get update -qq
 sudo apt-get -qq install --no-install-recommends qt59base
