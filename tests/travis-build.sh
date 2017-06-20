18e7d8349acaa57d803fb31eefcc618e44dbcebf
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-06-20 16:22:52 +0200

    .

--- tests/travis-build.sh
+++ tests/travis-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 echo QMAKE_CXX = g++-7 >> .qmake.conf
 
@@ -8,4 +8,9 @@ source /opt/qt*/bin/qt*-env.sh
 make qmake_all
 make -j$(nproc) all
 
+export LD_LIBRARY_PATH="$(pwd)/lib:$LD_LIBRARY_PATH"
 
+cd tests/auto
+for test in $(find . -type f -executable -name "tst_*"); do
+	$test
+done
