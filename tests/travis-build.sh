d329033309a2027c6b9d6c755c37005c0f3593da
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-06-20 17:15:40 +0200

    travis working

--- tests/travis-build.sh
+++ tests/travis-build.sh
@@ -10,7 +10,6 @@ make -j$(nproc) all
 
 export LD_LIBRARY_PATH="$(pwd)/lib:$LD_LIBRARY_PATH"
 
-set -x
 cd tests/auto
 for test in $(find . -type f -executable -name "tst_*"); do
 	QT_QPA_PLATFORM=minimal $test
