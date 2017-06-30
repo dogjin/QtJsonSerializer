ee936a0a82c4d0f81cb47c436138a3bab959701f
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-06-30 22:30:18 +0200

    fix sdk install & add jdk

--- tests/travis/linux/setup.sh
+++ tests/travis/linux/setup.sh
@@ -6,15 +6,16 @@ curl -Lo /tmp/android-sdk.zip https://dl.google.com/android/repository/sdk-tools
 mkdir android
 unzip -qq /tmp/android-sdk.zip -d android/sdk/
 echo y | ./android/sdk/tools/bin/sdkmanager --update
-echo y | ./android/sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-26" "build-tools;26.0.0" "extras;google;m2repository" "extras;android;support" "ndk-bundle" 
+echo y | ./android/sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-26" "build-tools;26.0.0" "extras;google;m2repository" "extras;android;m2repository" "ndk-bundle" 
 ./android/sdk/tools/bin/sdkmanager --list
 export ANDROID_HOME=$HOME/android/sdk
 export ANDROID_NDK_HOME=$HOME/android/sdk/ndk-bundle
- 
+
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test 
+sudo add-apt-repository --yes ppa:openjdk-r/ppa
 sudo apt-get -qq update
 
-sudo apt-get -qq install --no-install-recommends libgl1-mesa-dev libglib2.0-0 libpulse-dev make git ca-certificates curl xauth libx11-xcb1 libfontconfig1 libdbus-1-3 g++-7 python3 doxygen
+sudo apt-get -qq install --no-install-recommends libgl1-mesa-dev libglib2.0-0 libpulse-dev make git ca-certificates curl xauth libx11-xcb1 libfontconfig1 libdbus-1-3 g++-7 python3 doxygen openjdk-8-jdk
 
 curl -Lo /tmp/qpm https://www.qpm.io/download/v0.10.0/linux_386/qpm
 sudo install -m 755 /tmp/qpm /usr/local/bin/
