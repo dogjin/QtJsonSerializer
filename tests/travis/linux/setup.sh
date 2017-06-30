172517b408a57196edd0a62b26e9c492f2640f3e
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-06-30 22:21:42 +0200

    use cpp image & install via sdkmanager

--- tests/travis/linux/setup.sh
+++ tests/travis/linux/setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
 
-# android stuff
-sdkmanager --update
-sdkmanager --list
+# android skd/ndk
+curl -Lo /tmp/android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
+mkdir android
+unzip -qq /tmp/android-sdk.zip -d android/sdk/
+./android/sdk/tools/bin/sdkmanager --update
+echo y | ./android/sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-26" "build-tools;26.0.0" "extras;google;m2repository" "extras;android;support" "ndk-bundle" 
+./android/sdk/tools/bin/sdkmanager --list
 
+sudo mv /tmp/android-sdk /opt/android-sdk
+export ANDROID_HOME=$HOME/android/sdk
+export ANDROID_NDK_HOME=$HOME/android/sdk/ndk-bundle
+ 
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test 
 sudo apt-get -qq update
 
@@ -15,4 +23,4 @@ sudo install -m 755 /tmp/qpm /usr/local/bin/
 
 curl -Lo /tmp/installer.run https://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run
 chmod +x /tmp/installer.run
-QT_QPA_PLATFORM=minimal sudo /tmp/installer.run --script tests/travis/linux/qt-installer-script.qs --addRepository https://install.skycoder42.de/qtmodules/linux_x64
+QT_QPA_PLATFORM=minimal sudo /tmp/installer.run --script tests/travis/linuxy/qt-installer-script.qs --addRepository https://install.skycoder42.de/qtmodules/linux_x64
