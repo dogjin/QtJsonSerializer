5bc44524b6a7be1a244e2339c1ec737a9e06bc88
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-01 11:55:36 +0200

    fix sdk location

--- tests/travis/linux/setup.sh
+++ tests/travis/linux/setup.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
 
+# android skd/ndk
+curl -Lo /tmp/android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
+mkdir $HOME/android
+unzip -qq /tmp/android-sdk.zip -d $HOME/android/sdk/
+echo y | $HOME/android/sdk/tools/bin/sdkmanager --update
+echo y | $HOME/android/sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-26" "build-tools;26.0.0" "extras;google;m2repository" "extras;android;m2repository" "ndk-bundle"
+
+export ANDROID_HOME=$HOME/android/sdk
+export ANDROID_NDK=$HOME/android/sdk/ndk-bundle
+export ANDROID_SDK_ROOT=$ANDROID_HOME
+export ANDROID_NDK_ROOT=$ANDROID_NDK
+$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-g++ -v
+
 # install build deps
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test 
 sudo apt-get -qq update
@@ -15,17 +28,3 @@ sudo install -m 755 /tmp/qpm /usr/local/bin/
 curl -Lo /tmp/installer.run https://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run
 chmod +x /tmp/installer.run
 QT_QPA_PLATFORM=minimal sudo /tmp/installer.run --script tests/travis/linux/qt-installer-script.qs --addRepository https://install.skycoder42.de/qtmodules/linux_x64
-
-# android skd/ndk
-curl -Lo /tmp/android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
-mkdir android
-unzip -qq /tmp/android-sdk.zip -d android/sdk/
-echo y | ./android/sdk/tools/bin/sdkmanager --update
-echo y | ./android/sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-26" "build-tools;26.0.0" "extras;google;m2repository" "extras;android;m2repository" "ndk-bundle"
-
-# create root symlink (because env var is ignored?)
-opath=$HOME/android/sdk/ndk-bundle
-cd .. $opath
-cd $HOME
-sudo mkdir -p /opt/android
-sudo ln -s $opath /opt/android/ndk
