a1fa25f586576d88879ed600758290958c039d62
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-01 18:06:22 +0200

    use setlocal

--- tests/travis/win/build-msvc2017.bat
+++ tests/travis/win/build-msvc2017.bat
@@ -6,9 +6,9 @@ cd build-msvc2017
 C:\Qt\5.9\msvc2017_64\bin\qmake -r ..\qtjsonserializer.pro
 nmake all
 
-PATH=%PATH%;%CD%\lib;
+SETLOCAL PATH=%PATH%;%CD%\lib;
 cd tests\auto
-QT_QPA_PLATFORM=minimal
+SETLOCAL QT_QPA_PLATFORM=minimal
 for /r %%f in (tst_*.exe) do (
 	%%f || exit /B 1
 )
--- tests/travis/win/build.bat
+++ tests/travis/win/build.bat
@@ -2,8 +2,8 @@
 
 :: call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
-QTDIR=C:\Qt\5.9\msvc2017_64
-PATH=%PATH%;%QTDIR%\bin;C:\MinGW\bin;
+set QTDIR=C:\Qt\5.9\msvc2017_64
+set PATH=%PATH%;%QTDIR%\bin;C:\MinGW\bin;
 
 :: .\tests\travis\win\build-msvc2015.bat
 .\tests\travis\win\build-msvc2017.bat
