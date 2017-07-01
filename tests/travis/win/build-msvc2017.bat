73483c87c92c0d718c905d9c7a9b1932787c6534
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-01 16:24:24 +0200

    f1

--- tests/travis/win/build-msvc2017.bat
+++ tests/travis/win/build-msvc2017.bat
@@ -2,11 +2,11 @@ mkdir build-msvc2017
 cd build-msvc2017
 
 C:\Qt\5.9\msvc2017_64\bin\qmake -r ..\qtjsonserializer.pro
-jom.exe all
+nmake all
 
 cd tests\auto
 set QT_QPA_PLATFORM=minimal
 dir tst_*.exe /b/s
 
 cd ..\..
-jom.exe INSTALL_ROOT="$\tmp\install" install
+nmake INSTALL_ROOT="$\tmp\install" install
