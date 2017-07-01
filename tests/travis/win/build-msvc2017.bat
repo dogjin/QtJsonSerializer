5758f830ccfa732eda45e6946ac259b17b42a168
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-01 17:18:52 +0200

    .

--- tests/travis/win/build-msvc2017.bat
+++ tests/travis/win/build-msvc2017.bat
@@ -4,8 +4,10 @@ cd build-msvc2017
 C:\Qt\5.9\msvc2017_64\bin\qmake -r ..\qtjsonserializer.pro
 nmake all
 
+set PATH=%PATH%;%CD%\lib;
 cd tests\auto
 set QT_QPA_PLATFORM=minimal
+
 echo RUNNING TESTES
 C:\projects\qjsonserializer\build-msvc2017\tests\auto\jsonserializer\GadgetSerializerTest\debug\tst_gadgetserializer.exe
 echo tst_gadgetserializer %errorlevel%
