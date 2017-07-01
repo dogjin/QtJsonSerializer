d89a3077fb2e1318f2f2d1ae592ebe4ce3d754e7
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-01 18:54:27 +0200

    final try

--- tests/travis/win/build-msvc2015.bat
+++ tests/travis/win/build-msvc2015.bat
@@ -1,16 +1,17 @@
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-set PATH=C:\Qt\5.9\msvc2015_64\bin;%CD%\lib;%PATH%;
 
 mkdir build-msvc2015
 cd build-msvc2015
 
-qmake -r ..\qtjsonserializer.pro
+C:\Qt\5.9\msvc2015_64\bin\qmake -r ..\qtjsonserializer.pro
 nmake all
 
+set PATH=C:\Qt\5.9\msvc2015_64\bin;C:\Qt\5.9\msvc2015_64\lib;%CD%\lib;%PATH%;
 cd tests\auto
 set QT_QPA_PLATFORM=minimal
 for /r %%f in (tst_*.exe) do (
+	echo PATH: %PATH%
 	%%f || exit /B 1
 )
 
--- tests/travis/win/build-msvc2017.bat
+++ tests/travis/win/build-msvc2017.bat
@@ -1,14 +1,13 @@
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
-set PATH=C:\Qt\5.9\msvc2017_64\bin;%CD%\lib;%PATH%;
 
 mkdir build-msvc2017
 cd build-msvc2017
 
-qmake -r ..\qtjsonserializer.pro
+C:\Qt\5.9\msvc2017_64\bin\qmake -r ..\qtjsonserializer.pro
 nmake all
 
-set PATH=%PATH%;%CD%\lib;
+set PATH=C:\Qt\5.9\msvc2017_64\bin;%CD%\lib;%PATH%;
 cd tests\auto
 set QT_QPA_PLATFORM=minimal
 for /r %%f in (tst_*.exe) do (
