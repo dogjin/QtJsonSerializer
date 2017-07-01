fffa6558a7344c6e4c497346c50ed8d81459c118
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-01 17:22:01 +0200

    via loop

--- tests/travis/win/build-msvc2017.bat
+++ tests/travis/win/build-msvc2017.bat
@@ -7,12 +7,9 @@ nmake all
 set PATH=%PATH%;%CD%\lib;
 cd tests\auto
 set QT_QPA_PLATFORM=minimal
-
-echo RUNNING TESTES
-C:\projects\qjsonserializer\build-msvc2017\tests\auto\jsonserializer\GadgetSerializerTest\debug\tst_gadgetserializer.exe
-echo tst_gadgetserializer %errorlevel%
-C:\projects\qjsonserializer\build-msvc2017\tests\auto\jsonserializer\ObjectSerializerTest\debug\tst_objectserializer.exe
-echo tst_objectserializer %errorlevel%
+for /r %%f in (tst_*.exe) do (
+	%%f
+)
 
 cd ..\..
 nmake INSTALL_ROOT="\tmp\install" install
