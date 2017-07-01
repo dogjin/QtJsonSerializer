44a834f2dbe062e1850497665b2bdf96eaafa5ec
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-01 13:28:51 +0200

    ls more

--- tests/travis/osx/setup.sh
+++ tests/travis/osx/setup.sh
@@ -8,5 +8,5 @@ set -e
 # install Qt
 curl -Lo /tmp/installer.dmg https://download.qt.io/official_releases/online_installers/qt-unified-mac-x64-online.dmg
 hdiutil attach /tmp/installer.dmg
-ls /Volumes/
-QT_QPA_PLATFORM=minimal sudo /Volumes/installer/installer.app --script tests/travis/osx/qt-installer-script.qs --addRepository https://install.skycoder42.de/qtmodules/mac_x64/
+find /Volumes/qt-unified-mac-x64-3.0.0-online
+QT_QPA_PLATFORM=minimal sudo /Volumes/qt-unified-mac-*/installer.app --script tests/travis/osx/qt-installer-script.qs --addRepository https://install.skycoder42.de/qtmodules/mac_x64/
