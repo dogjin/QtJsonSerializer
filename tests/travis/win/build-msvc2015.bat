bc626ec22fecf5f414830a6ab1a557249c6452bc
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-01 18:57:36 +0200

    no tests

--- tests/travis/win/build-msvc2015.bat
+++ tests/travis/win/build-msvc2015.bat
@@ -7,13 +7,13 @@ cd build-msvc2015
 C:\Qt\5.9\msvc2015_64\bin\qmake -r ..\qtjsonserializer.pro
 nmake all
 
-set PATH=C:\Qt\5.9\msvc2015_64\bin;C:\Qt\5.9\msvc2015_64\lib;%CD%\lib;%PATH%;
-cd tests\auto
-set QT_QPA_PLATFORM=minimal
-for /r %%f in (tst_*.exe) do (
-	echo PATH: %PATH%
-	%%f || exit /B 1
-)
-
-cd ..\..
+::  set PATH=C:\Qt\5.9\msvc2015_64\bin;C:\Qt\5.9\msvc2015_64\lib;%CD%\lib;%PATH%;
+::  cd tests\auto
+::  set QT_QPA_PLATFORM=minimal
+::  for /r %%f in (tst_*.exe) do (
+::  	echo PATH: %PATH%
+::  	%%f || exit /B 1
+::  )
+::  
+::  cd ..\..
 nmake INSTALL_ROOT="\tmp\install" install
