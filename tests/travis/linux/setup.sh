c8101182600e0ba9d2c5f900b293f6612da3da56
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-01 15:02:57 +0200

    travis wait in yml

--- tests/travis/linux/setup.sh
+++ tests/travis/linux/setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-travis_wait echo test
-
 # install build deps
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test 
 sudo apt-get -qq update
--- tests/travis/osx/setup.sh
+++ tests/travis/osx/setup.sh
@@ -5,10 +5,8 @@ set -e
 curl -Lo /tmp/qpm https://www.qpm.io/download/v0.10.0/darwin_386/qpm
 sudo install -m 755 /tmp/qpm /usr/local/bin/
 
-travis_wait echo test
-
 # install Qt
 curl -Lo /tmp/installer.dmg https://download.qt.io/official_releases/online_installers/qt-unified-mac-x64-online.dmg
 hdiutil attach /tmp/installer.dmg
 find /Volumes/qt-unified-mac-x64-3.0.0-online
-QT_QPA_PLATFORM=minimal sudo travis_wait /Volumes/qt-unified-mac-*/qt-unified-mac-*/Contents/MacOS/qt-unified-mac-* --script tests/travis/osx/qt-installer-script.qs --addRepository https://install.skycoder42.de/qtmodules/mac_x64/
+QT_QPA_PLATFORM=minimal sudo /Volumes/qt-unified-mac-*/qt-unified-mac-*/Contents/MacOS/qt-unified-mac-* --script tests/travis/osx/qt-installer-script.qs --addRepository https://install.skycoder42.de/qtmodules/mac_x64/
