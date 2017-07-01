4286de36afff9eca0d2378ef51b3ac466752b19a
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-01 12:25:47 +0200

    gcc param

--- tests/travis/linux/build-gcc.sh
+++ tests/travis/linux/build-gcc.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
 
-echo QMAKE_CXX = g++-7 >> .qmake.conf
-
 mkdir build-gcc
 cd build-gcc
 
-/opt/qt/5.9.1/gcc_64/bin/qmake -r ../qtjsonserializer.pro
+/opt/qt/5.9.1/gcc_64/bin/qmake -r "QMAKE_CXX=g++-7" ../qtjsonserializer.pro
 make all
 make doxygen
 
