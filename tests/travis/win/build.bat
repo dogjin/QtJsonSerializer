0a02ba09760bf997034bb02a7cb474f8249b3d8a
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-01 17:50:40 +0200

    .

--- tests/travis/win/build.bat
+++ tests/travis/win/build.bat
@@ -5,7 +5,5 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary
 set QTDIR=C:\Qt\5.9\msvc2017_64
 set PATH=%PATH%;%QTDIR%\bin;C:\MinGW\bin;
 
-C:\Qt\MaintenanceTool.exe --help
-
-.\tests\travis\win\build-msvc2017.bat
 .\tests\travis\win\build-msvc2015.bat
+.\tests\travis\win\build-msvc2017.bat
