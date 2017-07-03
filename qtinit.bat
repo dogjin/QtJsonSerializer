a2a5748a94cbc08e3516233ecf790827f44822fb
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-03 19:55:42 +0200

    next try

--- qtinit.bat
+++ qtinit.bat
@@ -5,12 +5,12 @@ dir C:\Qt\Tools
 
 :: C:\Qt\MaintenanceTool.exe --silentUpdate || exit \B 1
 start C:\Qt\MaintenanceTool.exe --script ./qt-installer-modify-script.qs
-timeout /t 900
+for /l %%x in (1, 1, 5) do (
+	timeout /t 180
+	tasklist
+)
 
 dir C:\Qt
 dir C:\Qt\5.9.1
-
-tasklist
-taskkill /im C:\Qt\MaintenanceTool.exe /f
 
 exit 1
