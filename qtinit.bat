39a3dd58f97b382a556f6d0fd6034ad864ecb819
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-03 21:46:15 +0200

    test if running

--- qtinit.bat
+++ qtinit.bat
@@ -4,10 +4,22 @@ dir C:\Qt
 dir C:\Qt\Tools
 
 :: C:\Qt\MaintenanceTool.exe --silentUpdate || exit \B 1
-start timelog.bat
-C:\Qt\MaintenanceTool.exe --script ./qt-installer-modify-script.qs
+start C:\Qt\MaintenanceTool.exe --script ./qt-installer-modify-script.qs
+for /l %%x in (1, 1, 15) do (
+	timeout /t 60 > nul
+	tasklist /FI "IMAGENAME eq MaintenanceTool.exe" 2>NUL | find /I /N "MaintenanceTool.exe">NUL
+	if "%ERRORLEVEL%"=="0" (
+		echo Program is running
+	) else (
+		dir C:\Qt
+		dir C:\Qt\5.9.1
+		exit 1
+	)
+)
 
 dir C:\Qt
 dir C:\Qt\5.9.1
+
+echo TIMEOUT
 
 exit 1
--- timelog.bat
+++
@@ -1,6 +0,0 @@
-for /l %%x in (1, 1, 13) do (
-	timeout /t 60
-	tasklist
-)
-
-taskkill /im MaintenanceTool.exe /f
