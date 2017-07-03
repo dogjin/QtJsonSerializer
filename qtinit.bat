ebd5be5c9f4dc6abdb4c058968eed56cbc024183
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-03 12:11:49 +0200

    .

--- qtinit.bat
+++ qtinit.bat
@@ -3,8 +3,8 @@
 dir C:\Qt
 dir C:\Qt\Tools
 
+C:\Qt\MaintenanceTool.exe --silentUpdate
 C:\Qt\MaintenanceTool.exe --platform minimal --script ./qt-installer-modify-script.qs
-C:\Qt\MaintenanceTool.exe --platform minimal --silentUpdate
 
 dir C:\Qt
 
