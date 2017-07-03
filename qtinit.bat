c345b69c56bb4f97398ca3b1ee59e6bfe5f9d617
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-03 18:57:54 +0200

    try with taskkill to see state

--- qtinit.bat
+++ qtinit.bat
@@ -4,8 +4,15 @@ dir C:\Qt
 dir C:\Qt\Tools
 
 :: C:\Qt\MaintenanceTool.exe --silentUpdate || exit \B 1
-C:\Qt\MaintenanceTool.exe --script ./qt-installer-modify-script.qs || exit \B 1
+start C:\Qt\MaintenanceTool.exe --script ./qt-installer-modify-script.qs
+for /l %x in (1, 1, 15) do (
+	timeout /t 60
+	dir C:\Qt
+	dir C:\Qt\5.9.1
+)
+taskkill /im C:\Qt\MaintenanceTool.exe /f
 
 dir C:\Qt
+dir C:\Qt\5.9.1
 
 exit 1
