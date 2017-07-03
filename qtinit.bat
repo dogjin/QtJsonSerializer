f9e329a0f91b5fa32bdcdfd4fc5a60e402f95ac4
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-03 19:18:56 +0200

    check log

--- qtinit.bat
+++ qtinit.bat
@@ -5,14 +5,12 @@ dir C:\Qt\Tools
 
 :: C:\Qt\MaintenanceTool.exe --silentUpdate || exit \B 1
 start C:\Qt\MaintenanceTool.exe --script ./qt-installer-modify-script.qs
-for /l %%x in (1, 1, 15) do (
-	timeout /t 60
-	dir C:\Qt
-	dir C:\Qt\5.9.1
-)
-taskkill /im C:\Qt\MaintenanceTool.exe /f
+timeout /t 900
 
 dir C:\Qt
 dir C:\Qt\5.9.1
+type C:\Qt\InstallationLog.txt
+
+taskkill /im C:\Qt\MaintenanceTool.exe /f
 
 exit 1
