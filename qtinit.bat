8fb29fa5dfbd8724bb07bc12b85b64786b205750
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-03 12:10:57 +0200

    retry install first

--- qtinit.bat
+++ qtinit.bat
@@ -1,8 +1,10 @@
+@echo on
+
 dir C:\Qt
 dir C:\Qt\Tools
 
-C:\Qt\MaintenanceTool.exe --silentUpdate
 C:\Qt\MaintenanceTool.exe --platform minimal --script ./qt-installer-modify-script.qs
+C:\Qt\MaintenanceTool.exe --platform minimal --silentUpdate
 
 dir C:\Qt
 
