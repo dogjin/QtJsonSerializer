52527d79f49633a3654b77caad4fdfbe1f4f1ecc
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-07 12:01:43 +0200

    validate

--- qtinit.bat
+++ qtinit.bat
@@ -1,4 +1,6 @@
 @echo on
 
-:: C:\Qt\MaintenanceTool.exe --silentUpdate || exit \B 1
-C:\Qt\MaintenanceTool.exe --script ./qt-installer-modify-script.qs --addRepository https://install.skycoder42.de/qtmodules/windows_x86
+C:\Qt\MaintenanceTool.exe --silentUpdate || exit \B 1
+C:\Qt\MaintenanceTool.exe --script ./qt-installer-modify-script.qs --addRepository https://install.skycoder42.de/qtmodules/windows_x86 || exit \B 1
+
+dir C:\Qt\5.9\msvc2017_64\lib\*json*
